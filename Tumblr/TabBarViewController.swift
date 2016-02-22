f61011080a3dbd0864cd05fdfa441f4eefddf8f8
bhudelson <bhudelso@gmail.com>
2016-02-21 22:29:35 -0800

    Created main view controllers

---
+++ Tumblr/TabBarViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TabBarViewController.swift
+//  Tumblr
+//
+//  Created by Blake Hudelson on 2/21/16.
+//  Copyright © 2016 Blake Hudelson. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var composeButton: UIButton!
+    @IBOutlet weak var AccountButton: UIButton!
+    @IBOutlet weak var trendingButton: UIButton!
+    @IBOutlet weak var contentView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
